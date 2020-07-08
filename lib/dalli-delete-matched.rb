fa42c18693a98dbf6f2d1043a985246c0583a4d8
César Izurieta <cesar@caih.org>
2020-07-07 19:37:36 -0500

    Use mongoid for saving the list of keys

--- lib/dalli-delete-matched.rb
+++ lib/dalli-delete-matched.rb
@@ -3,61 +3,44 @@ require 'active_support/core_ext/module/aliasing'
 
 ActiveSupport::Cache::DalliStore.class_eval do
 
-  CACHE_KEYS = 'MemcachedCacheKeys'
-
   alias_method :old_write_entry, :write_entry
   def write_entry(key, entry, options)
-    keys = get_cache_keys
-    unless keys.include?(key)
-      keys << key
-      delete CACHE_KEYS
-      return false unless write_cache_keys(keys, options[:connection])
-    end
-    old_write_entry(key, entry, options)
+    ret = old_write_entry(key, entry, options)
+    doc = { hostnames: hostnames, key: key }
+    mongoid_memcached.update_one(doc, doc, { upsert: true })
+    ret
   end
 
   alias_method :old_delete_entry, :delete_entry
   def delete_entry(key, options)
     ret = old_delete_entry(key, options)
-    return false unless ret
-    keys = get_cache_keys
-    if keys.include?(key)
-      keys -= [key]
-      delete CACHE_KEYS
-      with { |c| write_cache_keys(keys, c) }
-    end
+    mongoid_memcached.delete_one({ hostnames: hostnames, key: key })
+    ret
+  end
+
+  alias_method :old_clear, :clear
+  def clear(options=nil)
+    ret = old_clear(options)
+    mongoid_memcached.delete_many({ hostnames: hostnames })
     ret
   end
 
   def delete_matched(matcher, options = {})
-    ret = true
-    deleted_keys = []
-    keys = get_cache_keys
-    keys.each do |key|
-      if ret && key.match(matcher)
-        deleted_keys << key if (ret = old_delete_entry(key, options))
-      end
-    end
-    len = keys.length
-    keys -= deleted_keys
-    if keys.length < len
-      with { |c| write_cache_keys(keys, c) }
-    end
-    ret
+    docs = mongoid_memcached.find({ hostnames: hostnames, key: /#{matcher}/ })
+    docs.each { |doc| old_delete_entry(doc[:key], options) }
+    mongoid_memcached.delete_many({ hostnames: hostnames, key: /#{matcher}/ }).deleted_count
   end
 
   private
 
-  def write_cache_keys(keys, connection)
-    old_write_entry(CACHE_KEYS, keys.to_yaml, { :connection => connection })
+  def mongoid_memcached
+    @mongoid_memcached ||= Mongoid.default_client.database[:memcached].tap do |mongoid_memcached|
+      mongoid_memcached.indexes.create_one({ hostnames: 1, key: 1 }, { unique: true })
+    end
   end
 
-  def get_cache_keys
-    begin
-      YAML.load read(CACHE_KEYS)
-    rescue TypeError
-      []
-    end
+  def hostnames
+    @hostnames ||= stats.keys.join(',')
   end
 
 end
